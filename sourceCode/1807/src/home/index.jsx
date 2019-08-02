a10ebbb04fa7b60b37ccb0e6c49297acdec43fa6
LAYadad <2556377311@qq.com>
2019-08-02 16:32:57 +0800

    react_uptate

--- sourceCode/1807/src/home/index.jsx
+++ sourceCode/1807/src/home/index.jsx
@@ -4,7 +4,7 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                酷酷的刘先生
+                酷酷的刘先生，我爱你
             </div>
         )
     }
