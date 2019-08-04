24834071a02f38b6870ff378902b588846efc466
lxs1008611 <3414739661@qq.com>
2019-08-04 23:11:41 +0800

    登录and注册

--- sourceCode/1807/src/index.js
+++ sourceCode/1807/src/index.js
@@ -5,9 +5,9 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 //引入的作业
-import Home from '../src/home';
+import Html from '../src/ly_wock/html';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<Html />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ sourceCode/1807/src/ly_wock/tool/index.js
@@ -0,0 +1,50 @@
+export default (param = {}, pandName) => {
+    const { userName, passWord, email, passWordEnd } = param;
+    switch (pandName) {
+        case 'zc':
+            if (userName === '' || userName.length < 11) {
+                return {
+                    status: false,
+                    msg: '您的手机号不能少于11位'
+                }
+            }
+            if (email === '' || email.length < 4) {
+                return {
+                    status: false,
+                    msg: '验证码不能少于4位'
+                }
+            }
+            if (passWord === '' || passWord.length < 6) {
+                return {
+                    status: false,
+                    msg: '您的密码不能为小于6位'
+                }
+            }
+            if (passWord !== passWordEnd) {
+
+                return {
+                    status: false,
+                    msg: '您的两次密码不相同'
+                }
+            }
+            return true;
+            
+    }
+    switch (pandName) {
+        case 'dl':
+
+            if (userName !== '13463115348' || userName.length < 11) {
+                return {
+                    status: false,
+                    msg: '您的手机号不能少于11位'
+                }
+            }
+            if (passWord !== '5211314lxs' || passWord.length < 6) {
+                return {
+                    status: false,
+                    msg: '您的密码名不能为小于6位'
+                }
+            }
+    }
+
+}
