4031da5108cd3231b647a27bf4abd11c0183d431
lxs1008611 <3414739661@qq.com>
2019-08-05 00:49:51 +0800

    abc

--- sourceCode/1807/src/index.js
+++ sourceCode/1807/src/index.js
@@ -5,16 +5,8 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 //引入的作业
-<<<<<<< HEAD
-import Html from './html/index';
-=======
-<<<<<<< HEAD
-// import Home from '../src/home';
-import Home from '../src/about';
-=======
 import Html from './html/index'
->>>>>>> 0df16362d735ed1c18b173c49d90e300955ebf9a
->>>>>>> 7ebf4dcd0919b70c1ac3237f55a5f6ca1d6eb65f
+
 
 ReactDOM.render(<Html />, document.getElementById('root'));
 
